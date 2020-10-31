fe93b906490864517e6e32ffa94508d349f783b0
Vincenzo Florio <vincenzoflorio31@gmail.com>
2020-10-31 12:40:43 +0000

    Add new desktop background and add html structure change

--- app.js
+++ app.js
@@ -1,53 +1,179 @@
-// Define User Interface Variables
-const form = document.querySelector("#task-form");
-const taskList = document.querySelector(".collection");
-const name = document.querySelector("#name");
-const description = document.querySelector("#description");
-const assignedTo = document.querySelector("#assignedTo");
-const dueDate = document.querySelector("#dueDate");
-const status = document.querySelector("#status");
-const errorElement = document.getElementById("error");
+// // Define User Interface Variables // Efent Listeners
+// const form = document.querySelector("#task-form"); // Targeting form
+// const taskList = document.querySelector(".collection"); // Targeting list of all tasks
+// const clearBtn = document.querySelector(".clear-tasks"); // Targets the clear button
+// const filter = document.querySelector("#filter");
+// const taskName = document.querySelector("#name"); // Targets the name input field
+// const taskDescription = document.querySelector("#description"); // Targets the description input field
+// const taskAssignedTo = document.querySelector("#assignedTo"); // Targets the assigned to input field
+// const taskDueDate = document.querySelector("#dueDate"); // Targets the due date input field
+// const taskStatus = document.querySelector("#status"); // Targets the status input field
+// const errorElement = document.getElementById("error"); // Targets the error message form validation
 
+// Book Constructor
+let idCounter = (function () {
+  let id = 1;
 
-form.addEventListener("submit", function(e){
-    let messages = []
-     // When form submit is clicked, performs function.
+  return function taskPlanner(name, description, assignedTo, dueDate, status) {
+    this.id = id++;
+    this.name = name;
+    this.description = description;
+    this.assignedTo = assignedTo;
+    this.dueDate = dueDate;
+    this.status = status;
+  };
+})();
 
-    // Name validation
-    if (name.value === "" || name.value == null) {
-        messages.push("Name is required")
-    }
+// UI Constructor
+function UI() {}
 
-    if (name.value.length < 8) {
-        messages.push("Please enter a name longer than 8 characters!")
-    }
+// Add task to list
+UI.prototype.addTaskToList = function (task) {
+  const taskCollection = document.getElementById("collection");
+  // Create custom list-group item
+  const divList = document.createElement("div");
+  // Add bootstrap class to div
+  divList.classList.add("list-group collection");
+  // Insert the custom list
+  divList.innerHTML = `
+  <a href="#" class="list-group-item list-group-item-action">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1 task-name">${task.name}</h5>
+                <small class="task-date">${task.dueDate}</small>
+              </div>
+              <p class="mb-1 task-description">${task.description}</p>
+              <small class="task-status">${task.status}</small>
+              <p>
+                <small class="task-assigned-to">${task.assignedTo}</small>
+              </p>
+              <span><i class="fas fa-times fa-lg"></i></span>
+            </a>
+  `;
 
-    // Description validation
-    if (description.value === "" || description.value == null) {
-        messages.push("Description is required")
-    }
+  taskCollection.appendChild(divList);
+};
 
-    if (description.value.length < 20) {
-        messages.push("Please enter a description longer than 20 characters!")
-    }
+//  Event Listeners
+document.querySelector("#task-form").addEventListener("submit", function (e) {
+  // Get Form values
+  const taskName = document.querySelector("#name").value;
+  const taskDescription = document.querySelector("#description").value;
+  const taskAssignedTo = document.querySelector("#assignedTo").value;
+  const taskDueDate = document.querySelector("#dueDate").value;
+  const taskStatus = document.querySelector("#status").value;
 
-    // Assigned To validation
+  // Instantiate a Book
+  const task = new idCounter(
+    taskName,
+    taskDescription,
+    taskAssignedTo,
+    taskDueDate,
+    taskStatus
+  );
 
-    if (assignedTo.value === "" || assignedTo.value == null) {
-        messages.push("Assigned to is required")
-    }
+  // Instantiate the UI Object
+  const ui = new UI();
 
-    if (assignedTo.value.length < 8) {
-        messages.push("Please enter an assigned to value longer than 8 characters!")
-    }
+  // Add task to list
+  ui.addTaskToList(task);
 
-    
-    if (messages.length > 0){
-        e.preventDefault(); // Prevent default form behaviours.
-        errorElement.innerText = messages.join(", ")
-    } 
+  e.preventDefault();
 });
 
+// form.addEventListener("submit", function (e) {
+//   let messages = [];
+//   // When form submit is clicked, performs function.
 
+//   // Name validation
+//   if (name.value === "" || name.value == null) {
+//     messages.push("Name is required");
+//   }
 
+//   if (name.value.length < 8) {
+//     messages.push("Please enter a name longer than 8 characters!");
+//   }
 
+//   // Description validation
+//   if (description.value === "" || description.value == null) {
+//     messages.push("Description is required");
+//   }
+
+//   if (description.value.length < 20) {
+//     messages.push("Please enter a description longer than 20 characters!");
+//   }
+
+//   // Assigned To validation
+
+//   if (assignedTo.value === "" || assignedTo.value == null) {
+//     messages.push("Assigned to is required");
+//   }
+
+//   if (assignedTo.value.length < 8) {
+//     messages.push(
+//       "Please enter an assigned to value longer than 8 characters!"
+//     );
+//   }
+
+//   if (messages.length > 0) {
+//     e.preventDefault(); // Prevent default form behaviours.
+//     errorElement.innerText = messages.join(", ");
+//   }
+// });
+
+// // Load all event listeners
+// loadEventListeners();
+
+// // Load all event listeners
+// function loadEventListeners() {
+//     // Add task event
+//     form.addEventListener("submit", addTask);
+// }
+
+// // Add Task
+// function addTask(e) {
+//     if(name.value === "") {
+//         alert("Add a task");
+//     }
+
+//     // Create list group item / task-name
+//     const liItem = document.createElement("h5");
+//     // Add class
+//     liItem.className = "list-group-item list-group-item-action";
+//     // Add dead link attribute
+//     liItem.setAttribute("href", "#");
+
+//     // Create div containing task name and task date.
+//     const nameDateDiv = document.createElement("div");
+//     // Add class
+//     nameDateDiv.className = "d-flex w-100 justify-content-between";
+
+//     // Create task-name h5
+//     const headingFive = document.createElement("h5");
+//     // Add class
+//     headingFive.className = "mb-1 task-name";
+
+//     // Create task-date small
+//     const smallName = document.createElement("small");
+//     // Add class
+//     smallName.className = "task-date";
+
+//     // Create description p
+//     const descriptionP = document.createElement("p");
+//     // Add class
+//     descriptionP.className = "mb-1 task-description";
+
+//     // Add task-status small
+//     const smallStatus = document.createElement("small");
+//     // Add class
+//     smallStatus.className = "task-status"
+
+//     // Create p which contains the assigned to small
+//     const taskAssignedP = document.createElement("p");
+
+//     // Create small to go into the p
+//     const taskAssignedSmall = document.createElement("small");
+//     // Add class
+//     taskAssignedSmall.className = "task-assigned-to";
+
+//     // e.preventDefault();
+// }
